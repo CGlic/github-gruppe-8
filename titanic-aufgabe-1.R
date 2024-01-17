11841fcb6f70b43770728be2bae389c3d3dd664d
KjellNoack <102241402+KjellNoack@users.noreply.github.com>
2024-01-17 15:43:02 +0100

    Update titanic-aufgabe-1.R
    Imputationsverfahren Variable Age hinzufegügt

--- titanic-aufgabe-1.R
+++ titanic-aufgabe-1.R
@@ -11,7 +11,8 @@ temp_split <- strsplit(temp_split, "\\.")
 temp_split <- unlist(lapply(temp_split, "[", c(1)))
 titanic$Salutation <- temp_split 
 rm(temp_split)
-´# Codierung der Variablen Sex, Survived,Embarkeds 
+
+# Codierung der Variablen Sex, Survived,Embarkeds 
 titanic$Sex <- as.factor(ifelse(titanic$Sex == "male", 1, 0))
 titanic$Survived <- as.factor(titanic$Survived)
 titanic$Embarked <- as.factor(titanic$Embarked)
@@ -19,11 +20,42 @@ titanic$Embarked <- as.factor(titanic$Embarked)
 # Pclass in einen ordered-factor überführen
 titanic$Pclass <- as.factor(titanic$Pclass)
 
-# fehlende Age-Werte imputieren durch arithmetisches Mittel
-    # Welches Verfahren wollen wir hier nutze?
+# fehlende Age-Werte imputieren durch Median der Anrede
   
-# fehlende Werte in Cabin zu NA's ändern
+    # Prüfen, bei welchen Anreden NA's vorhanden sind
+  unique(titanic$Salutation[which(is.na(titanic$Age))])
+    # Prüfen, welche Anreden existieren
+  unique(titanic$Salutation)
+  
+    #Mediane der gesuchten Anreden finden
+  med_age_mr <- median(titanic$Age[which(titanic$Salutation == "Mr")], na.rm = TRUE)
+          # hier möglichwerweise auch: Don, Rev, Major, Sir, Col, Capt, Jonkheer?
+  med_age_master <- median(titanic$Age[which((titanic$Salutation == "Master"))], na.rm = TRUE)
+  med_age_mrs <- median(titanic$Age[which((titanic$Salutation == "Mrs"))], na.rm = TRUE)
+          # hier möglicherweise auch: Lady, the Countess?
+  med_age_ms <- median(titanic$Age[titanic$Salutation %in% list("Ms", "Mlle", "Miss")], na.rm = TRUE)
+  med_age_dr <- median(titanic$Age[titanic$Salutation == "Dr"], na.rm = TRUE)
+  
+  
+    # Index der NAs finden             
+  age_na <- which(is.na(titanic$Age))
+  
+  index_na_mr <- intersect(age_na, which(titanic$Salutation %in% list("Mr")))
+  index_na_master <- intersect(age_na, which(titanic$Salutation %in% list("Master")))
+  index_na_mrs <- intersect(age_na, which(titanic$Salutation %in% list("Mrs")))
+  index_na_ms <- intersect(age_na, which(titanic$Salutation %in% list("Ms", "Mlle", "Miss")))
+  index_na_dr <- intersect(age_na, which(titanic$Salutation %in% list("Dr")))
+  
+    # NAs ersetzen
+  titanic$Age[index_na_mr] <- med_age_mr
+  titanic$Age[index_na_master] <- med_age_master
+  titanic$Age[index_na_mrs] <- med_age_mrs
+  titanic$Age[index_na_ms] <- med_age_ms
+  titanic$Age[index_na_dr] <- med_age_dr
+
+  # fehlende Werte in Cabin zu NA's ändern
 titanic$Cabin[which(titanic$Cabin == "")] <- NA
+  # Besondere Handhabung bei mehreren Kabinennummern? (s. z.B. Passagier Nr. 89)
 
 # Steuerbord und Backbord Variable hinzufügen
 temp_side <- as.numeric(gsub( ".*?([0-9]+).*", "\\1", titanic$Cabin))
